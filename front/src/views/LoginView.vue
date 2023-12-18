868e567e936be9a6a7c6a7e5a53cd88ae15d73b9
Andrea Saldaña Cavour <andrea.saldana@alumnos.ucn.cl>
2023-12-17 22:14:52 -0300

    feat: LoginView documentation

--- front/src/views/LoginView.vue
+++ front/src/views/LoginView.vue
@@ -1,33 +1,41 @@
 <template>
+  <!-- Página de inicio de sesión -->
   <ion-page>
     <ion-header>
+      <!-- Barra de herramientas con título de inicio de sesión -->
       <ion-toolbar>
         <ion-title>Iniciar Sesión</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content>
+      <!-- Formulario de inicio de sesión -->
       <form @submit.prevent="login" class="login-form">
+        <!-- Contenedor del formulario -->
         <div class="login-container">
+          <!-- Campo de correo electrónico -->
           <ion-item lines="full" class="ion-margin-bottom">
             <ion-label position="floating">Correo Electrónico</ion-label>
             <ion-input v-model="email" type="text" placeholder="Ingresa tu email"></ion-input>
           </ion-item>
 
+          <!-- Campo de contraseña -->
           <ion-item lines="full" class="ion-margin-bottom">
             <ion-label position="floating">Contraseña</ion-label>
-            <ion-input v-model="password" type="password"  placeholder="Ingresa tu contraseña"></ion-input>
+            <ion-input v-model="password" type="password" placeholder="Ingresa tu contraseña"></ion-input>
           </ion-item>
 
+          <!-- Botón para iniciar sesión -->
           <ion-button expand="block" type="submit" class="ion-margin-top">Iniciar sesión</ion-button>
         </div>
 
+        <!-- Enlace para registrarse -->
         <p class="register-link">¿No tienes cuenta? <a href="/register">Regístrate aquí</a></p>
       </form>
     </ion-content>
   </ion-page>
 </template>
-  
+
 <script>
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonButton, IonItem, IonLabel } from '@ionic/vue';
 import { defineComponent, ref } from 'vue';
@@ -35,11 +43,18 @@ import axios from 'axios';
 import router from '../router'; // Importa el enrutador de Vue Router
 import { UserStore } from '@/stores/user_state'; // Importa el almacenamiento local de usuario
 
-
-
-
+/**
+ * Componente Vue para la vista de inicio de sesión.
+ * Permite a los usuarios autenticarse en la aplicación.
+ *
+ * @component
+ */
 export default defineComponent({
   name: 'LoginView',
+
+  /**
+   * Registra los componentes Ionic utilizados.
+   */
   components: {
     IonContent,
     IonHeader,
@@ -50,15 +65,17 @@ export default defineComponent({
     IonButton,
     IonItem,
     IonLabel,
-
   },
+
   setup() {
     const mainStore = UserStore();
     const email = ref('');
     const password = ref('');
     const errors = ref('');
 
-
+    /**
+     * Navega hacia atrás en el historial del router.
+     */
     const goBack = () => {
       router.back();
     };
@@ -71,43 +88,41 @@ export default defineComponent({
       errors,
     };
   },
+
   methods: {
-    // Función para iniciar sesión
+    /**
+     * Realiza una solicitud de inicio de sesión al servidor y maneja la respuesta.
+     * Si es exitosa, guarda el token y los detalles del usuario en el almacenamiento
+     * local y redirige al usuario a la página principal.
+     */
     async login() {
       try {
+        // Solicitud de inicio de sesión al servidor
         const response = await axios.post('http://localhost:3000/api/user/login', {
           email: this.email,
           password: this.password,
         });
 
-        // Si la autenticación es exitosa, puedes guardar el token en el almacenamiento local o en una cookie
-        // También puedes redirigir al usuario a la página deseada (RepositoriesView en este caso)
+        // Procesamiento de la respuesta exitosa
         const token = response.data.token;
         const user = response.data.user;
         console.log('Token:', token);
-        console.log('Usuario:', response.data.user);
+        console.log('Usuario:', user);
         this.mainStore.login(user, token);
 
       } catch (error) {
+        // Manejo de errores durante la solicitud
         console.error('Error al iniciar sesión:', error);
-        console.log(error.response.data.message)
         if (error.response && error.response.data.message) {
           this.errors = error.response.data.message;
-          alert(this.errors)
+          alert(this.errors);
         };
-        // Maneja el error, muestra un mensaje al usuario, etc.
       }
     },
   },
 });
-
-
-
-
-
-
-
 </script>
+
   
 <style scoped>
 .login-form {
